1a9120587c1d78f01201a11350cfe4e4c690f6d0
Ricarlos Guimarães <ricarlosgp@gmail.com>
2023-09-11 20:16:41 -0300

    fix logo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,9 @@
-import logoFooter from '../../assets/LogoFooter.svg'
 import { Container } from './styles'
 
 export function Footer() {
   return (
     <Container>
-      <img src={logoFooter} alt="logo explorer" />
+      <img src='https://raw.githubusercontent.com/ricarlosgp/Food-Explorer-Frontend/6d709449466ef3934d0af6f23ab0ffb6434da69a/src/assets/logoFooter.svg' alt="logo explorer" />
 
       <p>© 2023 - Todos os direitos reservados.</p>
     </Container>
